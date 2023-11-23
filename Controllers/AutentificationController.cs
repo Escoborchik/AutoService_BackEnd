b7bc04807eba4fb896a1ea5f06257076d0e4aa92
Escoborchik <borovinskiy_2004@mail.ru>
2023-11-23 21:37:00 +0500

    Fixs

--- Controllers/AutentificationController.cs
+++ Controllers/AutentificationController.cs
@@ -11,30 +11,32 @@ namespace AutoService_BackEnd.Controllers
         [HttpPost]
         public IActionResult Autentificate([FromBody] DataUser dataUser, [FromServices] IClients reposclient)
         {
-            var answer = new AnswerAutentic();
+            //var answer = new AnswerAutentic();
+            int client_id = 0;
             var allclients = reposclient.GetClients();            
             foreach (var client in allclients)
             {
                 if (client.Phone.Equals(dataUser.Phone) && client.Password.Equals(dataUser.Password))
                 {
-                    answer.Client_id = client.Id;
-                    answer.Name = client.Name;
-                    answer.SurName = client.Surname;
-                    answer.SecondName = client.SecondName;
+                    //answer.Client_id = client.Id;
+                    //answer.Name = client.Name;
+                    //answer.SurName = client.Surname;
+                    //answer.SecondName = client.SecondName;
+                    client_id = client.Id;
                     break;
                 }
                 else
                 {
-                    answer.Client_id = -1;
+                    //answer.Client_id = -1;
                 }
             }
-            if (answer.Client_id == -1)
+            if (/*answer.Client_id == -1*/  client_id == -1)
             {
                 return new StatusCodeResult(401);
             }
             else
             {
-                return Ok(answer);
+                return Ok(/*answer*/client_id);
             }
             
         }
--- Program .cs
+++ Program .cs
@@ -17,6 +17,8 @@ builder.Services.AddTransient<IClients, ClientRepository>();
 builder.Services.AddTransient<ICars, CarRepository>();
 builder.Services.AddTransient<IOrders, OrderRepository>();
 builder.Services.AddTransient<IOrdersWithoutReg, OrderWithoutRegRepository>();
+builder.Services.AddTransient<IOrdersWithoutReg, OrderWithoutRegRepository>();
+ 
 
 var app = builder.Build();
 
@@ -28,9 +30,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
+app.UseCors(builder => builder
+   .AllowAnyOrigin()
+   .AllowAnyMethod()
+   .AllowAnyHeader());
+    
 app.MapControllers();
 
 app.Run();
